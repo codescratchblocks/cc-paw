489168c590b53e0e4d067d28d5fb7f347027e5ea
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-08-04 07:35:57 -0700

    fix #6 pre-1.0 upgrades

---
+++ releases/semver/1.2.1/pkg.lua
@@ -0,0 +1,11 @@
+{
+  name = "semver",
+  title = "Semantic Versioning",
+  description = "Semantic Versioning (http://semver.org/) for Lua.",
+  author = "Kikito",
+  version = "1.2.1",
+  confVersion = 2,
+  files = {
+    ["/lib/semver.lua"] = "../../../semver/1.2.1/semver.lua"
+  }
+}
