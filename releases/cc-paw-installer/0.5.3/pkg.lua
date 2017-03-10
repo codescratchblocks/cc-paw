4d5f0ebadb5997f784cbb8f019baa493cce736d2
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-03-09 16:58:30 -0800

    updated cc-paw-installer

---
+++ releases/cc-paw-installer/0.5.3/pkg.lua
@@ -0,0 +1,14 @@
+{
+  name = "cc-paw-installer",
+  title = "CC-PAW installer",
+  description = "An install script for the cc-paw package.",
+  author = "Guard13007 <paul.liverman.iii@gmail.com>",
+  license = "MIT",
+  source = "https://github.com/cc-paw/cc-paw",
+  version = "0.5.3",
+  confVersion = 2,
+  files = {
+    ["/bin/install-cc-paw"] = "installer.lua",
+    ["/usr/man/install-cc-paw"] = "manual.txt"
+  }
+}
