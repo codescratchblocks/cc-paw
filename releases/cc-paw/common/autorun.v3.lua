414edd34a48ebeb99c19e7e1310569f3ff014d6a
/Fox --develop <paul.liverman.iii@gmail.com>
2017-08-09 19:06:57 -0700

    Create autorun.v3.lua

---
+++ releases/cc-paw/common/autorun.v3.lua
@@ -0,0 +1,9 @@
+-- FHS directories for binaries and manual pages:
+shell.setPath(shell.path()..':/bin')
+help.setPath(help.path()..':/usr/man')
+-- man => help
+shell.setAlias("man", "help")
+-- remove temporary files
+if fs.isDir("/tmp") then
+ shell.run("rm", "/tmp/*")
+end
